471bb9ff535b69e378a1bbe805b03b06459ac092
Akku-7494 <akanshakhandelwal31@gmail.com>
2023-10-16 05:38:24 +0530

    ques3complete

--- dom.js
+++ dom.js
@@ -1,3 +1,35 @@
+// EXAMINE THE DOCUMENT OBJECT //
+// console.dir(document);
+// console.log(document.domain); 
+// console.log(document.URL) 
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype); 
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
 
-<script scr="dom.js"></script>
-console.dir(document);
+// GETELEMENTBYID //
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Hi';
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header')           //for pointing hearder
+//headerTitle.innerHTML ='<h3>Hello</h3>';
+// headerTitle.style.borderbottom = 'solid 3px #000';        for bottom only
+//headerTitle.style.border = 'solid 3px #000';                 // for complete
+//header.innerHTML ='<h3>Hello</h3>';
+header.style.border = 'solid 3px #000';                        // for complete border on header
+var addItem = document.getElementById('main');
+addItem.style.fontWeight = "bold";                              //changed to bold
+addItem.style.color = "#008000";                                // additem is now green
+
+
