ce0ef9bc1cac2bfeea66eb9f936e183e5c1de6b4
Akku-7494 <akanshakhandelwal31@gmail.com>
2023-10-19 06:06:09 +0530

    GetElementByClass

--- dom.js
+++ dom.js
@@ -32,4 +32,16 @@ var addItem = document.getElementById('main');
 addItem.style.fontWeight = "bold";                              //changed to bold
 addItem.style.color = "#008000";                                // additem is now green
 
+// GETELEMENTSBYCLASSNAME //
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style. fontweight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+for(var i = 0; i < items.length; i++){ 
+    items[i].style.backgroundColor = '#f4f4f4';
+}
 
