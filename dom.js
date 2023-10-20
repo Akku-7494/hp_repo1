c6dc6ee6816da4a64193a375a68e30b399d41557
Akku-7494 <akanshakhandelwal31@gmail.com>
2023-10-20 06:03:21 +0530

    GetElementsByTagName

--- dom.js
+++ dom.js
@@ -45,3 +45,16 @@ for(var i = 0; i < items.length; i++){
     items[i].style.backgroundColor = '#f4f4f4';
 }
 
+// GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontweight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+for (var i = 0; i < li.length; i++){
+li[i].style.backgroundColor = '#f4f4f4';
+}
+
