376a144f1b4ac7c2e1426ce45babd96ec80fdc5d
Akku-7494 <akanshakhandelwal31@gmail.com>
2023-10-29 23:23:53 +0530

    Child

--- dom.js
+++ dom.js
@@ -88,12 +88,18 @@
 // TRAVERSING THE DOM //
 var itemList = document.querySelector('#items');
 
-//parentNode
-console.log(itemList.parentNode);
-itemList.parentNode.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentNode.parentNode.parentNode);
+// //parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
 
-// parentElement
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentElement.parentElement.parentElement)
+// // parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement)
+
+// childNodes
+console.log(itemList.childNodes);
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor 'yellow';
